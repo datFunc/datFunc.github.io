ae26c74bd43303602ab0b3ac919ff47ea7aff80c
datFunc <m.a.m@msn.com>
2023-06-29 11:22:54 +0400

    Update btn_html5-trailers.user.js

--- btn/scripts/btn_html5-trailers.user.js
+++ btn/scripts/btn_html5-trailers.user.js
@@ -12,18 +12,11 @@
 // @run-at       document-end
 // ==/UserScript==
 
-( () => {
+(() => {
   'use strict';
 
-  // Select the node that will be observed for mutations
   const targetNode = document.getElementById('series');
-
-  // Options for the observer (which mutations to observe)
-  const config = {
-    attributes: false,
-    childList: true,
-    subtree: false,
-  };
+  const config = { attributes: false, childList: true, subtree: false };
 
   const applyStyling = (styles) => {
     const style = document.createElement('style');
@@ -34,81 +27,48 @@
   };
 
   applyStyling({
-    '#trailer': `
-        opacity: 1 !important;
-    `,
-    '#playbutton': `
-        opacity: 1;
-        cursor: pointer;
-        transition: 0.3s;
-    `,
-    '#trailerinner': `
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    `,
+    '#trailer': 'opacity: 1 !important;',
+    '#playbutton': 'opacity: 1; cursor: pointer; transition: 0.3s;',
+    '#trailerinner': 'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);',
   });
+
+  const disconnectObserver = (observer) => observer.disconnect();
 
   const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
-      if (mutation.type === 'childList') {
-        // Check for removed nodes
-        if (mutation.removedNodes.length) {
-          for (let i = 0; i < mutation.removedNodes.length; i++) {
-            // Check if the 'trailer' node was removed
-            if (
-              mutation.removedNodes[i] &&
-              mutation.removedNodes[i].id === 'trailer'
-            ) {
-              // Disconnect the observer when the 'trailer' node was removed
-              observer.disconnect();
-            }
-          }
-        }
+      if (mutation.type === 'childList' && mutation.removedNodes.length) {
+        Array.from(mutation.removedNodes).forEach( node => node.id === 'trailer' ? disconnectObserver(observer) : null );
+      }
 
-        if (
-          mutation.addedNodes.length > 0 &&
-          mutation.addedNodes[0].id === 'trailer'
-        ) {
-          const trailerContainerInnerContent = document.getElementById('trailerinner');
-          if (
-            trailerContainerInnerContent &&
-            trailerContainerInnerContent.childNodes.length > 0
-          ) {
-            const embedNode =
-              trailerContainerInnerContent.querySelector('#video > embed');
-            if (embedNode) {
-              const trailerURL = embedNode.getAttribute('src');
-              const iframeElement = `<iframe width="720" height="480" frameBorder="0" src=${trailerURL}></iframe>`;
-              trailerContainerInnerContent.innerHTML = iframeElement;
-            } else {
-              observer.disconnect();
-            }
-          } else {
-            observer.disconnect();
-          }
-        }
+      if (
+        mutation.addedNodes.length > 0 &&
+        mutation.addedNodes[0].id === 'trailer'
+      ) {
+        const trailerContainerInnerContent = document.getElementById('trailerinner');
+        if (trailerContainerInnerContent?.childNodes.length > 0) {
+          const embedNode = trailerContainerInnerContent.querySelector('#video > embed');
+          if (embedNode) {
+            trailerContainerInnerContent.innerHTML = `<iframe width="720" height="480" frameBorder="0" src=${embedNode.getAttribute('src')}></iframe>`;
+          } else disconnectObserver(observer);
+        } else disconnectObserver(observer);
       }
     }
   };
 
-  const observer = new MutationObserver(callback); // Create an observer instance linked to the callback function
-  observer.observe(targetNode, config); // Start observing the target node for configured mutations
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
 
-  // 'Sidebar' trailer
-  const sidebarBoxes = Array.from(document.querySelectorAll('.sidebar .box'));
-  const youtubeBox = sidebarBoxes.find((box) => {
+  const sidebarBoxes = document.querySelectorAll('.sidebar .box');
+  const youtubeBox = Array.from(sidebarBoxes).find((box) => {
     const headElement = box.querySelector('.head');
     return headElement && headElement.innerHTML.includes('Youtube');
   });
 
   if (youtubeBox) {
-    const embedNode = youtubeBox.querySelector('.nobullet > table > tbody > tr > td > object > embed');
+    const embedContainer = youtubeBox.querySelector('.nobullet > table > tbody > tr > td');
+    const embedNode = embedContainer.querySelector('object > embed');
     if (embedNode) {
-      const trailerURL = embedNode.getAttribute('src');
-      const iframeElement = `<iframe width="230" frameBorder="0" src=${trailerURL}></iframe>`;
-      youtubeBox.querySelector('.nobullet > table > tbody > tr > td').innerHTML = iframeElement;
+      embedContainer.innerHTML = `<iframe width="230" frameBorder="0" src=${embedNode.getAttribute('src')}></iframe>`;
     }
   }
 })();
