d12715c2fb6b25e58bc15505afcb9bb26ee2f73f
datFunc <m.a.m@msn.com>
2023-08-11 16:17:42 +0400

    Update btn_html5-trailers.user.js

--- btn/scripts/btn_html5-trailers.user.js
+++ btn/scripts/btn_html5-trailers.user.js
@@ -9,19 +9,17 @@
 // @updateURL    https://datfunc.github.io/btn/scripts/btn_html5-trailers.user.js
 // @downloadURL  https://datfunc.github.io/btn/scripts/btn_html5-trailers.user.js
 // @grant        none
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 (() => {
   'use strict';
 
-  const targetNode = document.getElementById('series');
-  const config = { attributes: false, childList: true, subtree: false };
-
   const applyStyling = (styles) => {
     const style = document.createElement('style');
     let styleContent = '';
-    for (let selector in styles) styleContent += `${selector} { ${styles[selector]} }`;
+    for (let selector in styles)
+      styleContent += `${selector} { ${styles[selector]} }`;
     style.innerHTML = styleContent;
     document.head.appendChild(style);
   };
@@ -29,7 +27,8 @@
   applyStyling({
     '#trailer': 'opacity: 1 !important;',
     '#playbutton': 'opacity: 1; cursor: pointer; transition: 0.3s;',
-    '#trailerinner': 'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);',
+    '#trailerinner':
+      'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);',
   });
 
   const disconnectObserver = (observer) => observer.disconnect();
@@ -37,26 +36,42 @@
   const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
       if (mutation.type === 'childList' && mutation.removedNodes.length) {
-        Array.from(mutation.removedNodes).forEach( node => node.id === 'trailer' ? disconnectObserver(observer) : null );
+        Array.from(mutation.removedNodes).forEach((node) => {
+          if (node.id === 'trailer') disconnectObserver(observer);
+        });
       }
 
       if (
         mutation.addedNodes.length > 0 &&
         mutation.addedNodes[0].id === 'trailer'
       ) {
-        const trailerContainerInnerContent = document.getElementById('trailerinner');
+        const trailerContainerInnerContent =
+          document.getElementById('trailerinner');
         if (trailerContainerInnerContent?.childNodes.length > 0) {
-          const embedNode = trailerContainerInnerContent.querySelector('#video > embed');
+          const embedNode =
+            trailerContainerInnerContent.querySelector('#video > embed');
           if (embedNode) {
-            trailerContainerInnerContent.innerHTML = `<iframe width="720" height="480" frameBorder="0" src=${embedNode.getAttribute('src')}></iframe>`;
+            trailerContainerInnerContent.innerHTML = `<iframe width="720" height="480" frameBorder="0" src=${embedNode.getAttribute(
+              'src'
+            )}></iframe>`;
           } else disconnectObserver(observer);
         } else disconnectObserver(observer);
       }
     }
   };
 
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+  const initObserver = () => {
+    const targetNode = document.getElementById('series');
+    if (!targetNode) {
+      setTimeout(initObserver, 500); // Retry after 500ms if #series is not found
+      return;
+    }
+    const config = { attributes: false, childList: true, subtree: false };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  };
+
+  initObserver();
 
   const sidebarBoxes = document.querySelectorAll('.sidebar .box');
   const youtubeBox = Array.from(sidebarBoxes).find((box) => {
@@ -65,10 +80,14 @@
   });
 
   if (youtubeBox) {
-    const embedContainer = youtubeBox.querySelector('.nobullet > table > tbody > tr > td');
+    const embedContainer = youtubeBox.querySelector(
+      '.nobullet > table > tbody > tr > td'
+    );
     if (embedContainer) {
       const embedNode = embedContainer.querySelector('object > embed');
-      embedContainer.innerHTML = `<iframe width="230" frameBorder="0" src=${embedNode.getAttribute('src')}></iframe>`;
+      embedContainer.innerHTML = `<iframe width="230" frameBorder="0" src=${embedNode.getAttribute(
+        'src'
+      )}></iframe>`;
     }
   }
 })();
