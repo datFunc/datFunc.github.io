c27c2213f067b961ce0e430507308528349d319b
datFunc <m.a.m@msn.com>
2023-08-15 20:22:30 +0400

    Update btn_html5-trailers.user.js

--- btn/scripts/btn_html5-trailers.user.js
+++ btn/scripts/btn_html5-trailers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BTN HTML5 Trailers
 // @namespace    http*://*broadcasthe.net/*
-// @version      0.1
+// @version      1.0
 // @description  "Fix" BTN's trailers by replacing the DOM old/flash with the new/HTML5 (iframe)
 // @author       PuNkFuSe
 // @match        https://www.broadcasthe.net/series.php?id=*
@@ -15,8 +15,6 @@
 (() => {
   'use strict';
 
-  console.log('Script started.');
-
   try {
     const applyStyling = (styles) => {
       const style = document.createElement('style');
@@ -28,10 +26,12 @@
 
     applyStyling({
       '#trailer': 'opacity: 1 !important;',
-      'img[id=banner]': 'transition: 0.3s',
-      '#playbutton': 'opacity: 0.65; cursor: pointer; transition: 0.3s; background: url(https://datfunc.github.io/ptp/assets/icons/play-trailer.svg); width: 110px !important; height: 110px !important; box-sizing: border-box !important; padding-left: 110px !important; background-size: contain !important; margin-top: 7px;',
-      '#series #content > div.thin > center:hover #playbutton': 'transform: scale(0.9); opacity: .75',
-      '#series #content > div.thin > center:hover img[id=banner]': 'opacity: .55',
+      '#banner': 'transition: 0.3s',
+      '#playbutton':
+        'opacity: 0.65; cursor: pointer; transition: 0.3s; background: url(https://datfunc.github.io/ptp/assets/icons/play-trailer.svg); width: 110px !important; height: 110px !important; box-sizing: border-box !important; padding-left: 110px !important; background-size: contain !important; margin-top: 7px;',
+      '#series #content center:hover #playbutton':
+        'transform: scale(0.9); opacity: .79',
+      '#series #content center:hover #banner': 'opacity: .65',
       '#trailerinner':
         'position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);',
     });
@@ -39,23 +39,14 @@
     console.error('Error in applyStyling:', error);
   }
 
-
-
-  const disconnectObserver = (observer) => {
-    console.log('Observer disconnected.');
-    observer.disconnect();
-  };
+  const disconnectObserver = (observer) => observer.disconnect();
 
   const callback = (mutationList, observer) => {
-    console.log('Mutation detected.');
     try {
       for (const mutation of mutationList) {
         if (mutation.type === 'childList' && mutation.removedNodes.length) {
           Array.from(mutation.removedNodes).forEach((node) => {
-            if (node.id === 'trailer') {
-              console.log("'trailer' node removed.");
-              disconnectObserver(observer);
-            }
+            node.id === 'trailer' ? disconnectObserver(observer) : null;
           });
         }
 
@@ -63,23 +54,19 @@
           mutation.addedNodes.length > 0 &&
           mutation.addedNodes[0].id === 'trailer'
         ) {
-          console.log("'trailer' node added.");
           const trailerContainerInnerContent =
             document.getElementById('trailerinner');
           if (trailerContainerInnerContent?.childNodes.length > 0) {
             const embedNode =
               trailerContainerInnerContent.querySelector('#video > embed');
             if (embedNode) {
-              console.log('Embed node found. Replacing with iframe.');
               trailerContainerInnerContent.innerHTML = `<iframe width="720" height="480" frameBorder="0" src=${embedNode.getAttribute(
                 'src'
               )}></iframe>`;
             } else {
-              console.log('Embed node not found.');
               disconnectObserver(observer);
             }
           } else {
-            console.log("'trailerinner' has no child nodes.");
             disconnectObserver(observer);
           }
         }
@@ -93,11 +80,9 @@
     try {
       const targetNode = document.getElementById('series');
       if (!targetNode) {
-        console.log('#series not found. Retrying in 500ms.');
         setTimeout(initObserver, 500);
         return;
       }
-      console.log('Initializing MutationObserver for #series.');
       const config = { attributes: false, childList: true, subtree: false };
       const observer = new MutationObserver(callback);
       observer.observe(targetNode, config);
@@ -116,7 +101,6 @@
     });
 
     if (youtubeBox) {
-      console.log('Youtube box found.');
       const embedContainer = youtubeBox.querySelector(
         '.nobullet > table > tbody > tr > td'
       );
@@ -126,8 +110,6 @@
           'src'
         )}></iframe>`;
       }
-    } else {
-      console.log('Youtube box not found.');
     }
   } catch (error) {
     console.error('Error in Youtube box processing:', error);
